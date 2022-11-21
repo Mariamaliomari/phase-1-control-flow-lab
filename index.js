26e9685e0e32b57364d95439c9d63a14e804b234
Omari99 <mariam.ali01@student.moringaschool.com>
2022-11-21 21:38:52 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
-function scuberGreetingForFeet(){
+
+function scuberGreetingForFeet(feet){
+  if(feet > 2500){
+    return('No can do.');}
+  if (feet <= 400){
+    return('This one is on me!');
+     }
+     else if(feet > 2000){
+    return ('I will gladly take your thirty bucks.');
+     }
+    if(feet > 2500){
+     return('No can do.');}
+}
   // Write your code here!
+
+
+function ternaryCheckCity(city){
+  return city==="NYC"?'Ok, sounds good.': 'No go.'
+  
+
+  
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+function switchOnCharmFromTip(tip){
+  if (tip === "generous"){
+    return('Thank you so much.');
+  }
+  else if(tip=== "not as generous"){
+    return ("Thank you.");
+  }
+  else {
+   return ("Bye.")
+  }
+  
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+  
+  
 }
